72346a5476f5863a3fc310b923735bed3f100d15
CamiNunes <cvn.camila@gmail.com>
2022-11-20 18:36:27 -0300

    criando validators;

---
+++ Controllers/AeronaveController.cs
@@ -0,0 +1,18 @@
+// using CiaAerea.Services;
+// using CiaAerea.ViewModels;
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace CiaAerea.Controllers
+// {
+//     [Route("api/aeronaves")]
+//     [ApiController] 
+//     public class AeronaveController  : ControllerBase
+//     {
+//         private readonly AeronaveService _aeronaveService;
+
+//         public AeronaveController(AeronaveService aeronaveService)
+//         {
+//             _aeronaveService = aeronaveService;
+//         }
+//     }
+// }
