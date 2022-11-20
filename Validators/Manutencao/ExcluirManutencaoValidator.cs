54b791c1522be331d71bb3ab8849cea5496fbeaa
CamiNunes <cvn.camila@gmail.com>
2022-11-20 18:40:14 -0300

    criando validators;

---
+++ Validators/Manutencao/ExcluirManutencaoValidator.cs
@@ -0,0 +1,20 @@
+using CiaAerea.Contexts;
+using FluentValidation;
+
+namespace CiaAerea.Validators.Manutencao
+{
+    public class ExcluirManutencaoValidator : AbstractValidator<int>
+    {
+        private readonly CiaAereaContext _context;
+
+        public ExcluirManutencaoValidator(CiaAereaContext context)
+        {
+            _context = context;
+
+            RuleFor(id => _context.Manutencoes.Find(id))
+                .Cascade(CascadeMode.Stop)
+                .NotNull().WithMessage("Id da manutenção inválido.")
+                .Must(manutencao => manutencao!.DataHora > DateTime.Now).WithMessage("Não é possível excluir uma manutenção já realizada.");
+        }    
+    }
+}
